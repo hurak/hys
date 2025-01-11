212fd1c58123ed6e6339ee84921c66fb916eedaf
Zdeněk Hurák <hurak@fel.cvut.cz>
2025-01-11 14:03:47 +0100

    The state at the end of the Fall semester 2024/25.

---
+++ hybrid_system_course_mindmap.md
@@ -0,0 +1,49 @@
+# B(E)3M35HyS
+
+## Discrete-event systems 
+
+1. (State) automata (state machines) (and timed automata)
+2. Petri nets (and timed Petri nets), 
+3. Max-Plus algebra and Max-Plus Linear (MPL) systems
+
+## Modelling hybrid systems
+
+4. Hybrid automata
+5. Hybrid equations
+
+## 6. Special classes of hybrid systems
+
+- Reset (control) systems
+- Switched/switching systems 
+- Piecewise affine systems (PWA) 
+
+## 7. Numerical simulation of hybrid systems
+
+- Complementarity dynamical systems (and complementarity optimization constraints)    
+
+## Analysis of hybrid systems
+
+8. Solutions of hybrid systems
+
+- Stability of hybrid systems
+
+    9. Common Lyapunov function 
+
+        - Quadratic Lyapunov function via linear matrix inequality (LMI) and semidefinite programming (SDP) 
+        - Polynomial Lyapunov function via sum-of-squares (SOS) programming
+    
+    10. Piecewise quadratic/polynomial Lyapunov function via S-procedure / Putinar's Positivstellensatz
+
+## Control design
+    
+11. Mixed-logical dynamical (MLD) description of hybrid systems
+12. Model predictive control (MPC) for MLD systems 
+
+## 13. Verification of hybrid systems
+
+- Reachability analysis
+    - (In)feasibility of an optimal control problem
+    - Set propagation techniques
+- (Control) barrier functions
+- Temporal logics 
+    - LTL, CTL, CTL*, MTL, MITL, TCTL*, STL, ...
---
+++ playground.jl
@@ -0,0 +1,56 @@
+using SumOfSquares
+using DynamicPolynomials
+using MosekTools
+using CSDP
+
+optimizer = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true)
+model = SOSModel(optimizer)
+@polyvar x[1:2] 
+
+p = 1;
+
+f = [ x[2],
+     -x[1] + (p/3)*x[1]^3 - x[2]]
+
+g₁ = -(x[1]+1)^2 - (x[2]+1)^2 + 0.16  # 𝒳ᵤ = {x ∈ R²: g₁(x) ≥ 0}
+h₁ = -(x[1]-1.5)^2 - x[2]^2 + 0.25    # 𝒳₀ = {x ∈ R²: h₁(x) ≥ 0}
+
+X = monomials(x, 0:4)
+@variable(model, B, Poly(X))
+
+ε = 0.001
+@constraint(model, B >= ε, domain = @set(g₁ >= 0))
+@constraint(model, B <= 0, domain = @set(h₁ >= 0))
+
+using LinearAlgebra # Needed for `dot`
+dBdt = dot(differentiate(B, x), f)
+@constraint(model, -dBdt >= 0)
+
+optimize!(model)
+
+import DifferentialEquations, Plots, ImplicitPlots
+function phase_plot(f, B, g₁, h₁, quiver_scaling, Δt, X0, solver = DifferentialEquations.Tsit5())
+    X₀plot = ImplicitPlots.implicit_plot(h₁; xlims=(-2, 3), ylims=(-2.5, 2.5), resolution = 1000, label="X₀", linecolor=:blue)
+    Xᵤplot = ImplicitPlots.implicit_plot!(g₁; xlims=(-2, 3), ylims=(-2.5, 2.5), resolution = 1000, label="Xᵤ", linecolor=:teal)
+    Bplot  = ImplicitPlots.implicit_plot!(B; xlims=(-2, 3), ylims=(-2.5, 2.5), resolution = 1000, label="B = 0", linecolor=:red)
+    Plots.plot(X₀plot)
+    Plots.plot!(Xᵤplot)
+    Plots.plot!(Bplot)
+    ∇(vx, vy) = [fi(x[1] => vx, x[2] => vy) for fi in f]
+    ∇pt(v, p, t) = ∇(v[1], v[2])
+    function traj(v0)
+        tspan = (0.0, Δt)
+        prob = DifferentialEquations.ODEProblem(∇pt, v0, tspan)
+        return DifferentialEquations.solve(prob, solver, reltol=1e-8, abstol=1e-8)
+    end
+    ticks = -5:0.5:5
+    X = repeat(ticks, 1, length(ticks))
+    Y = X'
+    Plots.quiver!(X, Y, quiver = (x, y) -> ∇(x, y) / quiver_scaling, linewidth=0.5)
+    for x0 in X0
+        Plots.plot!(traj(x0), idxs=(1, 2), label = nothing)
+    end
+    Plots.plot!(xlims = (-2, 3), ylims = (-2.5, 2.5), xlabel = "x₁", ylabel = "x₂")
+end
+
+phase_plot(f, value(B), g₁, h₁, 10, 30.0, [[x1, x2] for x1 in 1.2:0.2:1.7, x2 in -0.35:0.1:0.35])
